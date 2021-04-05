8f041f2bb94e516737ec4f400a108d75204a7c92
Lidiia Plys <reinali@yahoo.com>
2021-04-05 13:39:14 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+//alert('Fight!')
+
+//console.log('Fight!');
+
+let firstName = 'Scorpio';
+firstName = 'One'
+console.log(firstName);
+const lastName = 'Blade'
+console.log(lastName);
+
+console.log(typeof +'4');
+console.log('hello' + 'world');
+const hello = 'Hello'
+const world = 'world'
+const concat = hello + world
+console.log(concat);
+console.log(concat + 5);

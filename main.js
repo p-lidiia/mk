cbed94e0a8b39055ba1e881cdded2a52a134889e
Lidiia Plys <reinali@yahoo.com>
2021-04-08 14:23:49 +0300

    homework-2
    хз, верно ли, мучалась долго, вроде что-то вышло))

--- main.js
+++ main.js
@@ -1,17 +1,69 @@
-//alert('Fight!')
 
-//console.log('Fight!');
+ const player1 = {
+    name: 'Subzero',
+    hp: 86,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+    weapon: ['Devil`s whip', 'Hammer from hell'],
+     attack: function (){
+        console.log('Subzero' + ' ' + ' , fight!')
+    }
+}
+player1.attack()
 
-let firstName = 'Scorpio';
-firstName = 'One'
-console.log(firstName);
-const lastName = 'Blade'
-console.log(lastName);
 
-console.log(typeof +'4');
-console.log('hello' + 'world');
-const hello = 'Hello'
-const world = 'world'
-const concat = hello + world
-console.log(concat);
-console.log(concat + 5);
+ const player2 = {
+    name: 'Liu Kang',
+    hp: 53, 
+    img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
+    weapon: ['Sable'],
+    attack: function (){
+        console.log('Liu Kang' + ' ' + ' , fight!')
+    }
+}
+player2.attack()
+
+function createPlayer(player, playObj) {
+
+
+ const $arenas = document.querySelector('.arenas')
+
+const $player1 = document.createElement('div')
+$arenas.appendChild($player1)
+$player1.classList.add(player)
+
+
+/*  const $player2 = document.createElement('div')
+$arenas.appendChild($player2)
+$player2.classList.add('player2')
+$player2.className = 'player2'  */
+
+
+const $progressBar = document.createElement('div')
+$player1.appendChild($progressBar)
+$progressBar.classList.add('progressbar')
+
+const $character = document.createElement('div')
+$player1.appendChild($character)
+$character.classList.add('character')
+
+const $life = document.createElement('div')
+$progressBar.appendChild($life)
+$life.classList.add('life')
+$life.style.width = playObj.hp + '%'
+
+const $name = document.createElement('div')
+$progressBar.appendChild($name)
+$name.classList.add('name')
+$name.innerText = playObj.name
+
+const $img = document.createElement('img')
+$character.appendChild($img)
+$img.classList.add('character_img')
+$img.src = playObj.img;
+
+}
+
+console.log(createPlayer);
+createPlayer('player1', player1);
+createPlayer('player2', player2);
+
